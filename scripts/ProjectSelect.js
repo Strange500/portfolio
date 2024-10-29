6921aa8bed7000f0d02a859063d112015aa3017c
benjamin Roget <benjamin.roget.etu@univ-lille.fr>
2024-10-29 14:00:16 +0100

    new projects

--- scripts/ProjectSelect.js
+++ scripts/ProjectSelect.js
@@ -13,13 +13,40 @@ function selectProject(projectId) {
 }
 
 function unselectAllProjects() {
+    // opacity transition effect to make selected project disappear
     let divs = projectContainer.querySelectorAll("div");
-    divs.forEach((div) => {
-        div.classList.remove("invisible");
-    });
+
+    setTimeout(() => {
+        for (let i = 0; i < projectIds.length; i++) {
+                document.getElementById(projectIds[i]).classList.add("out");
+                console.log("out");
+        }
+        divs.forEach((div) => {
+            div.classList.add("out");
+        });
+    }, 500);
+
+    setTimeout(() => {
     projectIds.forEach((id) => {
-    document.getElementById(id).classList.remove("selected");
-  });
+        document.getElementById(id).classList.remove("selected");
+    });
+    setTimeout(() => {
+        divs.forEach((div) => {
+            div.classList.remove("invisible");
+        });
+    }, 250);
+
+        divs.forEach((div) => {
+            div.classList.remove("out");
+        });
+
+        setTimeout(() => {
+    for (let i = 0; i < projectIds.length; i++) {
+            document.getElementById(projectIds[i]).classList.remove("out");
+            console.log("in");
+    }
+        }, 100); // Small delay to ensure smooth transition
+    }, 1000);
 }
 
 
